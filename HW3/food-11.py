32bbba957f97bcfdb0947f77676aae4c4ad71e3c
Marco-Ni <670743258@qq.com>
2022-05-22 21:08:06 +0800

    food-11修改后

--- HW3/food-11.py
+++ HW3/food-11.py
@@ -4,44 +4,45 @@ import torch
 import torch.nn as nn
 import torchvision.transforms as transforms
 from PIL import Image
-# "ConcatDataset" and "Subset" are possibly useful when doing semi-supervised learning.
+# 在进行半监督学习时，“ConcatDataset”和“子集”可能是有用的。
+from tensorboardX import SummaryWriter
 from torch.utils.data import ConcatDataset, DataLoader, Subset, dataloader
 from torchvision.datasets import DatasetFolder
 
-# This is for the progress bar.
+# 进度条
 from tqdm.auto import tqdm
 
 # It is important to do data augmentation in training.
 # However, not every augmentation is useful.
 # Please think about what kind of augmentation is helpful for food recognition.
 train_tfm = transforms.Compose([
-    # Resize the image into a fixed shape (height = width = 128)
+    # 将图像调整为固定形状(高度=宽度= 128)
     transforms.Resize((128, 128)),
     # You may add some transforms here.
-    # ToTensor() should be the last one of the transforms.
+    # ToTensor() 应该是最后一个转换。
     transforms.ToTensor(),
 ])
 
-# We don't need augmentations in testing and validation.
-# All we need here is to resize the PIL image and transform it into Tensor.
+# 我们不需要增加测试和验证。
+# 我们需要做的就是调整PIL图像的大小，并将其转换为张量。
 test_tfm = transforms.Compose([
     transforms.Resize((128, 128)),
     transforms.ToTensor(),
 ])
 
-# Batch size for training, validation, and testing.
-# A greater batch size usually gives a more stable gradient.
-# But the GPU memory is limited, so please adjust it carefully.
+# 用于培训、验证和测试的批大小。
+# 更大的批大小通常提供更稳定的梯度。
+# 批处理较大的批处理。但是GPU内存有限，请仔细调整。
 batch_size = 128
 
-# Construct datasets.
-# The argument "loader" tells how torchvision reads the data.
+# 构建数据集。
+# 参数“loader”告诉了torchvision如何读取数据
 train_set = DatasetFolder("food-11/training/labeled", loader=lambda x: Image.open(x), extensions="jpg", transform=train_tfm)
 valid_set = DatasetFolder("food-11/validation", loader=lambda x: Image.open(x), extensions="jpg", transform=test_tfm)
 unlabeled_set = DatasetFolder("food-11/training/unlabeled", loader=lambda x: Image.open(x), extensions="jpg", transform=train_tfm)
 test_set = DatasetFolder("food-11/testing", loader=lambda x: Image.open(x), extensions="jpg", transform=test_tfm)
 
-# Construct data loaders.
+# 构建数据加载器。
 train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=2, pin_memory=True)
 valid_loader = DataLoader(valid_set, batch_size=batch_size, shuffle=True, num_workers=2, pin_memory=True)
 test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False)
@@ -49,11 +50,11 @@ test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False)
 class Classifier(nn.Module):
     def __init__(self):
         super(Classifier, self).__init__()
-        # The arguments for commonly used modules:
+        # 常用模块的参数:
         # torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
         # torch.nn.MaxPool2d(kernel_size, stride, padding)
 
-        # input image size: [3, 128, 128]
+        # 输入图片大小 [3, 128, 128]
         self.cnn_layers = nn.Sequential(
             nn.Conv2d(3, 64, 3, 1, 1),
             nn.BatchNorm2d(64),
@@ -68,55 +69,65 @@ class Classifier(nn.Module):
             nn.Conv2d(128, 256, 3, 1, 1),
             nn.BatchNorm2d(256),
             nn.ReLU(),
-            nn.MaxPool2d(4, 4, 0),
+            nn.MaxPool2d(2, 2, 0),
+
+            nn.Conv2d(256, 512, 3, 1, 1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2, 0),
+
+            nn.Conv2d(512, 512, 3, 1, 1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2, 0),
         )
         self.fc_layers = nn.Sequential(
-            nn.Linear(256 * 8 * 8, 256),
+            nn.Linear(512 * 4 * 4, 1024),
             nn.ReLU(),
-            nn.Linear(256, 256),
+            nn.Linear(1024, 512),
             nn.ReLU(),
-            nn.Linear(256, 11)
+            nn.Linear(512, 11)
         )
 
     def forward(self, x):
         # input (x): [batch_size, 3, 128, 128]
         # output: [batch_size, 11]
 
-        # Extract features by convolutional layers.
+        # 通过卷积层提取特征。
         x = self.cnn_layers(x)
 
-        # The extracted feature map must be flatten before going to fully-connected layers.
+        # 提取的特征映射必须在进行全连接层之前进行平坦处理。
         x = x.flatten(1)
 
-        # The features are transformed by fully-connected layers to obtain the final logits.
+        # 通过全连通层对特征进行变换，得到最终的对数。
         x = self.fc_layers(x)
         return x
 
 def get_pseudo_labels(dataset, model, threshold=0.65):
-    # This functions generates pseudo-labels of a dataset using given model.
-    # It returns an instance of DatasetFolder containing images whose prediction confidences exceed a given threshold.
-    # You are NOT allowed to use any models trained on external data for pseudo-labeling.
+    # 这个函数使用给定的模型生成数据集的伪标签。
+    # 它返回一个DatasetFolder实例，其中包含预测置信度超过给定阈值的图像。
+    # 不允许使用任何经过外部数据训练的模型进行伪标记。
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
-    # Make sure the model is in eval mode.
+    # 确保模型处于eval模式。
     model.eval()
-    # Define softmax function.
+    # 定义softmax函数。
     softmax = nn.Softmax(dim=-1)
 
-    # Iterate over the dataset by batches.
+    # 按批次遍历数据集。
     for batch in tqdm(dataloader):
         img, _ = batch
 
-        # Forward the data
-        # Using torch.no_grad() accelerates the forward process.
+        # 提出了数据
+        # 使用 torch.no_grad() 加速前进过程。
         with torch.no_grad():
             logits = model(img.to(device))
 
-        # Obtain the probability distributions by applying softmax on logits.
+        # 通过对logits应用softmax获得概率分布。
         probs = softmax(logits)
 
         # ---------- TODO ----------
-        # Filter the data and construct a new dataset.
+        # 筛选数据并构建新的数据集。
 
     # # Turn off the eval mode.
     model.train()
@@ -125,135 +136,145 @@ def get_pseudo_labels(dataset, model, threshold=0.65):
 # "cuda" only when GPUs are available.
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-# Initialize a model, and put it on the device specified.
+# 初始化一个模型，并将其放在指定的设备上。
 model = Classifier().to(device)
 model.device = device
 
-# For the classification task, we use cross-entropy as the measurement of performance.
+# 对于分类任务，我们使用交叉熵作为性能的度量。
 criterion = nn.CrossEntropyLoss()
 
-# Initialize optimizer, you may fine-tune some hyperparameters such as learning rate on your own.
-optimizer = torch.optim.Adam(model.parameters(), lr=0.0003, weight_decay=1e-5)
+# 初始化优化器，您可以自己微调一些超参数，如学习速率。
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=1e-5)
 
-# The number of training epochs.
+# 训练期的数量。
 n_epochs = 80
 
-# Whether to do semi-supervised learning.
+# 是否进行半监督学习。
 do_semi = False
+
+# 添加tensorboard
+writer = SummaryWriter("/logs")
 
 for epoch in range(n_epochs):
     # ---------- TODO ----------
-    # In each epoch, relabel the unlabeled dataset for semi-supervised learning.
-    # Then you can combine the labeled dataset and pseudo-labeled dataset for the training.
+    # 在每个时期，为半监督学习重新标记未标记的数据集。
+    # 然后，您可以结合标记数据集和伪标记数据集进行培训。
     if do_semi:
-        # Obtain pseudo-labels for unlabeled data using trained model.
+        # 对未标注数据使用训练模型获得伪标签。
         pseudo_set = get_pseudo_labels(unlabeled_set, model)
 
-        # Construct a new dataset and a data loader for training.
-        # This is used in semi-supervised learning only.
+        # 构建一个新的数据集和一个用于培训的数据加载器。
+        # 这只用于半监督学习。
         concat_dataset = ConcatDataset([train_set, pseudo_set])
         train_loader = DataLoader(concat_dataset, batch_size=batch_size, shuffle=True, num_workers=8, pin_memory=True)
 
     # ---------- Training ----------
-    # Make sure the model is in train mode before training.
+    # 在训练前确保模型处于训练模式。
     model.train()
 
-    # These are used to record information in training.
+    # 记录训练信息的。
     train_loss = []
     train_accs = []
 
-    # Iterate the training set by batches.
+    # 按批次迭代训练集。
     for batch in tqdm(train_loader):
 
-        # A batch consists of image data and corresponding labels.
+        # 批处理由图像数据和相应的标签组成。
         imgs, labels = batch
 
-        # Forward the data. (Make sure data and model are on the same device.)
+        # 提出了数据。(确保数据和模型在同一台设备上。)
         logits = model(imgs.to(device))
 
-        # Calculate the cross-entropy loss.
-        # We don't need to apply softmax before computing cross-entropy as it is done automatically.
+        # 计算交叉熵损失。
+        # 我们不需要在计算交叉熵之前应用softmax，因为它是自动完成的。
         loss = criterion(logits, labels.to(device))
 
-        # Gradients stored in the parameters in the previous step should be cleared out first.
+        # 应该首先清除上一步中存储在参数中的渐变。
         optimizer.zero_grad()
 
-        # Compute the gradients for parameters.
+        # 计算参数的梯度。
         loss.backward()
 
-        # Clip the gradient norms for stable training.
+        # 修剪梯度规范的稳定训练。
         grad_norm = nn.utils.clip_grad_norm_(model.parameters(), max_norm=10)
 
-        # Update the parameters with computed gradients.
+        # 使用计算的梯度更新参数。
         optimizer.step()
 
-        # Compute the accuracy for current batch.
+        # 计算当前批处理的精度。
         acc = (logits.argmax(dim=-1) == labels.to(device)).float().mean()
 
-        # Record the loss and accuracy.
+        # 记录损失和准确性。
         train_loss.append(loss.item())
         train_accs.append(acc)
 
-    # The average loss and accuracy of the training set is the average of the recorded values.
+    # 训练集的平均损耗和精度是记录值的平均值。
     train_loss = sum(train_loss) / len(train_loss)
     train_acc = sum(train_accs) / len(train_accs)
+    writer.add_scalar("train_loss", train_loss.item(), epoch)
+    writer.add_scalar("train_acc", train_acc.item(), epoch)
 
-    # Print the information.
+    # 打印信息。
     print(f"[ Train | {epoch + 1:03d}/{n_epochs:03d} ] loss = {train_loss:.5f}, acc = {train_acc:.5f}")
 
     # ---------- Validation ----------
-    # Make sure the model is in eval mode so that some modules like dropout are disabled and work normally.
+    # 确保该模型处于eval模式，以便某些模块(如dropout)被禁用并正常工作。
     model.eval()
 
-    # These are used to record information in validation.
+    # 它们用于记录验证中的信息。
     valid_loss = []
     valid_accs = []
 
-    # Iterate the validation set by batches.
+    # 按批次迭代验证集。
     for batch in tqdm(valid_loader):
 
-        # A batch consists of image data and corresponding labels.
+        # 批处理由图像数据和相应的标签组成。
         imgs, labels = batch
 
-        # We don't need gradient in validation.
-        # Using torch.no_grad() accelerates the forward process.
+        # 我们不需要在验证中使用梯度。
+        # 使用torch.no_grad()可以加速向前进程
         with torch.no_grad():
           logits = model(imgs.to(device))
 
         # We can still compute the loss (but not the gradient).
         loss = criterion(logits, labels.to(device))
 
-        # Compute the accuracy for current batch.
+        # 计算当前批处理的精度。
         acc = (logits.argmax(dim=-1) == labels.to(device)).float().mean()
 
-        # Record the loss and accuracy.
+        # 记录损失和准确性。
         valid_loss.append(loss.item())
         valid_accs.append(acc)
 
-    # The average loss and accuracy for entire validation set is the average of the recorded values.
+    # 整个验证集的平均损失和准确性是记录值的平均值。
     valid_loss = sum(valid_loss) / len(valid_loss)
     valid_acc = sum(valid_accs) / len(valid_accs)
+    writer.add_scalar("valid_loss", valid_loss.item(), epoch)
+    writer.add_scalar("valid_acc", valid_acc.item(), epoch)
 
     # Print the information.
     print(f"[ Valid | {epoch + 1:03d}/{n_epochs:03d} ] loss = {valid_loss:.5f}, acc = {valid_acc:.5f}")
 
-# Make sure the model is in eval mode.
-# Some modules like Dropout or BatchNorm affect if the model is in training mode.
+    torch.save(model.state_dict(),"food{}.pth".format(epoch))
+
+# 确保模型处于eval模式。
+# 有些模块如Dropout或BatchNorm会影响模型是否处于训练模式。
 model.eval()
 
-# Initialize a list to store the predictions.
+# 初始化一个列表来存储预测。
 predictions = []
 
-# Iterate the testing set by batches.
+# 按批次迭代测试集
 for batch in tqdm(test_loader):
-    # A batch consists of image data and corresponding labels.
-    # But here the variable "labels" is useless since we do not have the ground-truth.
-    # If printing out the labels, you will find that it is always 0.
-    # This is because the wrapper (DatasetFolder) returns images and labels for each batch,
-    # so we have to create fake labels to make it work normally.
+    # 批处理由图像数据和相应的标签组成。
+    # 但在这里，变量“标签”是无用的，因为我们没有基本真理。
+    # 如果打印出标签，你会发现它总是0。
+    # 这是因为包装器(DatasetFolder)
+    # 为每个批返回图像和标签，
+    # 所以我们必须制作假标签来让它正常工作。
     imgs, labels = batch
 
-    # We don't need gradient in testing, and we don't even have labels to compute loss.
+    # 在测试中我们不需要梯度，我们甚至没有标签来计算损失。
     # Using torch.no_grad() accelerates the forward process.
     with torch.no_grad():
         logits = model(imgs.to(device))
@@ -262,7 +283,7 @@ for batch in tqdm(test_loader):
     predictions.extend(logits.argmax(dim=-1).cpu().numpy().tolist())
 
 # Save predictions into the file.
-with open("predict.csv", "w") as f:
+with open("predicte1.csv", "w") as f:
 
     # The first row must be "Id, Category"
     f.write("Id,Category\n")
