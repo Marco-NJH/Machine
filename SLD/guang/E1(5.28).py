8cc6dfa9149293c70a1c24d889a81b31f20956fe
MarcoNJH <670743258@qq.com>
2023-05-28 23:47:06 +0800

    5.28

---
+++ SLD/guang/E1(5.28).py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+# 公式E1
+data1 = pd.read_excel('计算反射(5.28).xls', sheet_name='Sheet2', usecols=[0, 1], header=0, nrows=441)
+data2 = pd.read_excel('计算反射(5.28).xls', sheet_name='Sheet2', usecols=[2, 3], header=0, nrows=441)
+lamda1 = data1.iloc[:, 0].values
+lamda2 = data2.iloc[:,0].values
+R = data1.iloc[:, 1].values
+I = data2.iloc[:, 1].values
+
+A = 0  # I积分
+B = 0  # R*I积分
+for i in range(440):
+    ds = (lamda2[i + 1] - lamda2[i]) * I[i]
+    A += ds
+
+F = R * I
+for i in range(440):
+    ds = (lamda1[i + 1] - lamda1[i]) * F[i]
+    B += ds
+
+R_averge = B / A
+print(R_averge)
